ebb53455da8b674f2ee40aeb016b7fea5c30105a
arl480 <arl480@nyu.edu>
2021-11-26 13:04:17 -0500

    moved components into a theme

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -16,6 +16,7 @@ import {
   Avatar,
   Divider,
   IconButton,
+  Paper
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
@@ -52,11 +53,7 @@ const Navbar = ({ handleClick, isLoggedIn }) => {
             <Toolbar>
               <IconButton
               onClick={handleBurger}
-                size="large"
-                edge="start"
-                color="inherit"
                 aria-label="menu"
-                sx={{ mr: 2 }}
               >
                 <MenuIcon />
               </IconButton>
@@ -65,42 +62,12 @@ const Navbar = ({ handleClick, isLoggedIn }) => {
                 open={openBurger}
                 onClose={handleCloseBurger}
                 onClick={handleCloseBurger}
-                PaperProps={{
-                  elevation: 0,
-                  sx: {
-                    overflow: "visible",
-                    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                    mt: 1.5,
-                    "& .MuiAvatar-root": {
-                      width: 32,
-                      height: 32,
-                      ml: -0.5,
-                      mr: 10,
-                    },
-                    "&:before": {
-                      content: '""',
-                      display: "block",
-                      position: "absolute",
-                      top: 0,
-                      right: 87,
-                      width: 10,
-                      height: 10,
-                      bgcolor: "background.paper",
-                      transform: "translateY(-50%) rotate(45deg)",
-                      zIndex: 0,
-                      sx: {
-                        p: 50,
-                      }
-                    },
-                  },
-                }}
-                transformOrigin={{ horizontal: "right", vertical: "top" }}
-                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
               >
+              <Paper>
                 <MenuItem>
                   <Link to="/levelup">Level Up</Link>
                 </MenuItem>
-                
+
                 <Divider />
                 <MenuItem>
                   <Link to="/lookingfor">Looking For</Link>
@@ -110,6 +77,7 @@ const Navbar = ({ handleClick, isLoggedIn }) => {
                 
                 <Link to="/messages">Messages</Link>
                 </MenuItem>
+                </Paper>
               </Menu>
               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                 <Link to="/home">Code Harmony</Link>
@@ -119,9 +87,7 @@ const Navbar = ({ handleClick, isLoggedIn }) => {
                   {/* The navbar will show these links after you log in */}
                   <IconButton
                     onClick={handleMenu}
-                    size="large"
                     aria-label="user"
-                    color="inherit"
                   >
                     <AccountCircleIcon />
                   </IconButton>
@@ -130,36 +96,8 @@ const Navbar = ({ handleClick, isLoggedIn }) => {
                     open={open}
                     onClose={handleClose}
                     onClick={handleClose}
-                    className="test"
-                    PaperProps={{
-                      elevation: 0,
-                      sx: {
-                        overflow: "visible",
-                        filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                        mt: 1.5,
-                        "& .MuiAvatar-root": {
-                          width: 32,
-                          height: 32,
-                          ml: -0.5,
-                          mr: 1,
-                        },
-                        "&:before": {
-                          content: '""',
-                          display: "block",
-                          position: "absolute",
-                          top: 0,
-                          right: 14,
-                          width: 10,
-                          height: 10,
-                          bgcolor: "background.paper",
-                          transform: "translateY(-50%) rotate(45deg)",
-                          zIndex: 0,
-                        },
-                      },
-                    }}
-                    transformOrigin={{ horizontal: "right", vertical: "top" }}
-                    anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
                   >
+                    <Paper>
                     <MenuItem>
                       <Avatar /> Profile
                     </MenuItem>
@@ -169,31 +107,32 @@ const Navbar = ({ handleClick, isLoggedIn }) => {
                     <Divider />
                     <MenuItem>
                       <ListItemIcon>
-                        <PersonAdd fontSize="small" />
+                        <PersonAdd />
                       </ListItemIcon>
                       Add another account
                     </MenuItem>
                     <MenuItem>
                       <ListItemIcon>
-                        <Settings fontSize="small" />
+                        <Settings />
                       </ListItemIcon>
                       Settings
                     </MenuItem>
                     <MenuItem onClick={handleClick}>
                       <ListItemIcon>
-                        <Logout fontSize="small" />
+                        <Logout />
                       </ListItemIcon>
                       Logout
                     </MenuItem>
+                    </Paper>
                   </Menu>
                 </div>
               ) : (
                 <div>
                   {/* The navbar will show these links before you log in */}
-                  <Button color="inherit">
+                  <Button>
                     <Link to="/login">Login</Link>
                   </Button>
-                  <Button color="inherit">
+                  <Button>
                     <Link to="/signup">Sign Up</Link>
                   </Button>
                 </div>
