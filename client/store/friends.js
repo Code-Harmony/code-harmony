e740577bfb3c3e7ec645be31244f36cd23fa5661
Stephen Wong <stephen_wong_tx@icloud.com>
2021-12-06 17:43:39 -0600

    commit

--- client/store/friends.js
+++ client/store/friends.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { auth } from './auth';
+
 
 const LOAD_FRIENDS = 'LOAD_FRIENDS';
 
@@ -9,9 +11,34 @@ const friendsReducers = (state = [], action) =>{
     return state;
 }
 
+//const {accounts, friends} = this.props;
+  /*
+    let firstFriendId = friends.find((user) => user.user1id === currentUser.id)
+    let secondFriendId = firstFriendId.find((user) => user.user2id === currentUser.id)
+    let friendslist = accounts.find((friend) => friend.id === secondFriendId.id)
+  */
+
 const loadFriends = () =>{
     return async (dispatch) =>{
-        const friends = (await axios.get('/api/friend')).data;
+        const friendRelations = (await axios.get('/api/friend')).data;
+        const accounts = (await axios.get('/api/account')).data;
+
+        const userOneFriends = friendRelations.filter( ({ user1id }) => auth.id === user1id.id);
+        const userTwoFriends = friendRelations.filter( ({ user2id }) => auth.id === user2id.id);
+
+        const friendIds = userTwoFriends.reduce((result, userTwo) => {
+            if (userOneFriends.findIndex( userOneFriend => userOneFriend.user2id === userTwo.user1id.id) !== -1) {
+                result.push(userTwo.user1id.id)
+            } 
+            return result;
+        },[]);
+        const friends = accounts.reduce( (result, account) => {
+            if (friendIds.findIndex( friendId => friendId === account.id) !== -1) {
+                result.push(account)
+            }
+            return result;
+        },[]);
+
         dispatch(_loadFriends(friends));
     }
 }
